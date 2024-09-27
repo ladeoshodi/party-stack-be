4952d6aa12bfa4895e4f66286dfcec832c5edab6
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-09-27 10:18:09 +0100

    Convert app to a serverless app for netlify

---
+++ netlify/functions/api.ts
@@ -0,0 +1,39 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import swaggerOutput from "../../src/swagger-output.json";
+import swaggerUi from "swagger-ui-express";
+import { routes } from "../../src/routes";
+import { MONGODB_URI } from "../../src/config/environment";
+import serverless from "serverless-http";
+
+// middlewares
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/api", routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+
+// error handling
+app.use((e: any, req: Request, res: Response, next: NextFunction) => {
+  if (e.status >= 400 && e.status <= 499) {
+    res.status(e.status).json({ message: e.message });
+  } else {
+    console.error(e);
+    res
+      .status(500)
+      .json({ message: "An error occured, please try again later" });
+  }
+});
+
+async function start() {
+  await mongoose.connect(MONGODB_URI);
+  console.log("Connected to the database:", MONGODB_URI);
+}
+
+start();
+
+export const handler = serverless(app);
